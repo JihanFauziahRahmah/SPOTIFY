d47ff76b34121dd5b616145da2cc25339e24bb67
JihanFauziahRahmah <jihanfauziahrr@gmail.com>
2025-11-25 16:25:16 +0700

    Update spotify.h

--- spotify.h
+++ spotify.h
@@ -1,14 +1,18 @@
 #ifndef SPOTIFY_H_INCLUDED
 #define SPOTIFY_H_INCLUDED
 
+#include <string>
+using namespace std;
+
 struct Song {
     int id;
-    char title[];
-    char artist[];
-    char genre[];
+    string title;
+    string artist;
+    string genre;
 };
 
 typedef struct SongNode *adrSong;
+
 struct SongNode {
     Song info;
     adrSong next;
@@ -29,7 +33,7 @@ struct RelationNode {
 };
 
 struct PlaylistNode {
-    char name[];
+    string name;       
     adrRelation firstSong;
     adrPlaylist next;
 };
@@ -50,32 +54,29 @@ struct PlayQueue {
     adrQueue tail;
 };
 
-void removeSongFromQueue(PlayQueue &Q, adrSong songToRemove)
+void removeSongFromQueue(PlayQueue &Q, adrSong songToRemove);
+
 void createSongList(SongList &L);
 adrSong allocateSong(Song x);
 void insertSongLast(SongList &L, adrSong p);
 void deleteSongById(SongList &L, int id, adrSong &p, PlaylistList &PL, PlayQueue &Q);
+
 adrSong searchSongById(SongList L, int id);
-adrSong searchSongByTitle(SongList L, char title[]);
+adrSong searchSongByTitle(SongList L, string title);   
+
 void updateSongData(adrSong p, Song newData);
 void printSongList(SongList L);
+
 void createPlaylistList(PlaylistList &PL);
-adrPlaylist allocatePlaylist(char name[]);
+adrPlaylist allocatePlaylist(string name);            
 void insertPlaylistLast(PlaylistList &PL, adrPlaylist p);
-void deletePlaylistByName(PlaylistList &PL, char name[], adrPlaylist &p);
-adrPlaylist searchPlaylistByName(PlaylistList PL, char name[]);
+void deletePlaylistByName(PlaylistList &PL, string name, adrPlaylist &p);
+adrPlaylist searchPlaylistByName(PlaylistList PL, string name);
+
 void addSongToPlaylist(adrPlaylist P, adrSong S);
 void removeSongFromPlaylist(adrPlaylist P, int songId);
 void printSongsInPlaylist(adrPlaylist P);
-void cleanupSongReferences(PlaylistList &PL, adrSong deletedSong);
-void createPlaylistList(PlaylistList &PL);
-adrPlaylist allocatePlaylist(char name[]);
-void insertPlaylistLast(PlaylistList &PL, adrPlaylist p);
-void deletePlaylistByName(PlaylistList &PL, char name[], adrPlaylist &p);
-adrPlaylist searchPlaylistByName(PlaylistList PL, char name[]);
-void addSongToPlaylist(adrPlaylist P, adrSong S);
-void removeSongFromPlaylist(adrPlaylist P, int songId);
-void printSongsInPlaylist(adrPlaylist P);
+
 void cleanupSongReferences(PlaylistList &PL, adrSong deletedSong);
 
-#endif // SPOTIFY_H_INCLUDED
+#endif
